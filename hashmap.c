174b1ad900698818a1a8ce768f5cbfebdcee6e60
FranGPUCV <147253714+panquemadito@users.noreply.github.com>
2024-04-25 14:48:16 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -156,7 +156,7 @@ Pair * searchMap(HashMap * map,  char * key) {
 }
 
 Pair * firstMap(HashMap * map) {
-  if (map == NULL) return NULL;
+  if (map != NULL) return NULL;
   for (long i = 0; i < map->capacity; i++) {
     if (map->buckets[i] != NULL && map->buckets[i]->key != NULL) {
       map->current = i;
